9d1155ace2edeff626beeffe92110d8fa64b6253
Queren Neves dos Santos <80592870+querenneves@users.noreply.github.com>
2023-03-15 23:37:34 -0300

    Update Exercicio_03.java

--- src/exercicios/Exercicio_03.java
+++ src/exercicios/Exercicio_03.java
@@ -1,10 +1,35 @@
 package exercicios;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exercicio_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("###,###,##0.00");
+	
+		
+		float salarioBruto, noturno, horasExtras, descontos, salarioLiquido;	
+		
+		System.out.println("Digite o Salário Bruto: ");
+		salarioBruto = leia.nextFloat();
+		System.out.println("Digite o Adicional Noturno: ");
+		noturno = leia.nextFloat();
+		System.out.println("Digite as Horas Extras: ");
+		horasExtras = leia.nextFloat();
+		System.out.println("Digite os Descontos: ");
+		descontos = leia.nextFloat();
+		
+		salarioLiquido = salarioBruto + noturno + (horasExtras * 5) - descontos;
+		
+		System.out.println("O salário liquido é: " + df.format(salarioLiquido));
+		
+		leia.close();
+		
+	
 
 	}
 
 }
+
