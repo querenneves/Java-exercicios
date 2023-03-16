dc15590e9bd05c7b3638d44dd5f54c371474b14a
Queren Neves dos Santos <80592870+querenneves@users.noreply.github.com>
2023-03-15 23:37:02 -0300

    Update Exercicio_02.java

--- src/exercicios/Exercicio_02.java
+++ src/exercicios/Exercicio_02.java
@@ -1,10 +1,34 @@
 package exercicios;
 
-public class Exercicio_02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    import java.util.Scanner;
 
-	}
+    public class Exercicio_02{
 
-}
+        public static void main(String[] args) {
+
+        float numero1, numero2, numero3, numero4;
+        Scanner leia = new Scanner(System.in);
+
+
+        System.out.println("Digite a primeira nota");
+        numero1 = leia.nextFloat();
+
+        System.out.println("Digite a segunda nota");
+        numero2 = leia.nextFloat();
+
+        System.out.println("Digite a terceira note");
+        numero3 = leia.nextFloat();
+
+        System.out.println("Digite a quarta nota");
+        numero4 = leia.nextFloat();
+
+        System.out.println("Media : " + ((numero1 + numero2 + numero3 + numero4) / 4));
+
+
+
+
+        leia.close();
+
+    }
+    }
