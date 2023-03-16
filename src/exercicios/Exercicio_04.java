c05d56e9afe07d35487a5a57aa8173520dea2ecc
Queren Neves dos Santos <80592870+querenneves@users.noreply.github.com>
2023-03-15 23:38:07 -0300

    Update Exercicio_04.java

--- src/exercicios/Exercicio_04.java
+++ src/exercicios/Exercicio_04.java
@@ -1,10 +1,33 @@
 package exercicios;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Exercicio_04 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	float numero1, numero2, numero3, numero4, calculo;
+	Scanner leia = new Scanner(System.in);
+			
+	System.out.println("Digite o primeiro numero");
+	numero1 = leia.nextFloat();
+	
+	System.out.println("Digite o segundo numero");
+	numero2 = leia.nextFloat();
+	
+	System.out.println("Digite o terceiro numero");
+	numero3 = leia.nextFloat();
+	
+	System.out.println("Digite o quarto numero");
+	numero4 = leia.nextFloat();
+	
+	calculo = (numero1 * numero2)- (numero3 * numero4);
+	
+	System.out.println("O calculo é: " + (calculo));
+	
+	leia.close();
 
-	}
-
+}
 }
